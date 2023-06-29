fd0dc4ea6fecd9fa03fa1bd593e90b6a5ffd80bd
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2023-06-29 13:40:02 -0400

    Initial Commit

---
+++ BodyBloom/BodyBloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyBloomApp.swift
+//  BodyBloom
+//
+//  Created by Kiara on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct BodyBloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
