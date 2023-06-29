08c7eb58eb2dcecf82fb0ffc5a312791b3edf3ee
Liz Hernandez <lizcarol2005@gmail.com>
2023-06-29 14:23:02 -0400

    iTrial 2

--- BodyBloom/ContentView.swift
+++ BodyBloom/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("pls work")
+            Text("pleaseeee")
         }
         .padding()
     }
